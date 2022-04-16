43d2a51fc4b86fa4b376b4dad41456745df7b1bf
rokib-ru-cse <rokibahmed.ru.cse@gmail.com>
2022-04-16 12:53:12 +0600

    adding new code

--- DFSEdgeDeletion.java
+++ DFSEdgeDeletion.java
@@ -1,16 +1,22 @@
 import java.util.*;
 
 
-
-
 public class DFSEdgeDeletion {
     private static int nodes;
     private static int edges;
     private static ArrayList<Integer>[] adjclt;
+    private static int[] sum;
+    private static int[] weight;
 
     private static void initializedAdjcl() {
         for (int i = 1; i <= nodes; i++) {
             adjclt[i] = new ArrayList<>();
+        }
+    }
+
+    private static void initializedSum(){
+        for (int i = 1; i <=nodes; i++) {
+            sum[i] = 0;
         }
     }
 
@@ -24,12 +30,21 @@ public class DFSEdgeDeletion {
             adjclt[y].add(x);
         }
     }
+    private static void getWeight(){
+        Scanner in = new Scanner(System.in);
+        for (int i = 1; i <=nodes; i++) {
+            weight[i] = in.nextInt();
+        }
+    }
 
-    private static void dfs(int parent, int child) {
+    private static void dfs(int parent, int vertex) {
 
-        for (int i : adjclt[child]) {
-            if (parent != i) {
-                dfs(child, i);
+        sum[vertex] += weight[vertex];
+
+        for (int child : adjclt[vertex]) {
+            if (parent != child) {
+                dfs(vertex, child);
+                sum[vertex] += sum[child];
             }
         }
     }
@@ -39,8 +54,22 @@ public class DFSEdgeDeletion {
         nodes = in.nextInt();
         edges = in.nextInt();
         adjclt = new ArrayList[nodes + 1];
+        sum = new int[nodes+1];
+        weight = new int[nodes+1];
         initializedAdjcl();
+        initializedSum();
         getTree();
+        getWeight();
         dfs(0, 1);
+        for (int i = 1; i <=nodes; i++) {
+            System.out.print(sum[i]+" ");
+        }
+        int ans = 0;
+        for (int i = 2; i <= nodes; ++i) {
+            int part1 = sum[i];
+            int part2 = sum[1]-sum[i];
+            ans  = Math.max(ans,(part1*part2));
+        }
+        System.out.println("\nMaximum product is : "+ans);
     }
 }
